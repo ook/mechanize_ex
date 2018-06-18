4a8c9b05fea0b816b1de558e5bf5d4d63d44816b
Thomas Lecavelier <ook@ookook.fr>
2018-06-18 18:34:25 +0200

    mechanize_ex init + httpoison dependency

---
+++ lib/mechanize_ex.ex
@@ -0,0 +1,18 @@
+defmodule MechanizeEx do
+  @moduledoc """
+  Documentation for MechanizeEx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MechanizeEx.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mechanize_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule MechanizeExTest do
+  use ExUnit.Case
+  doctest MechanizeEx
+
+  test "greets the world" do
+    assert MechanizeEx.hello() == :world
+  end
+end
